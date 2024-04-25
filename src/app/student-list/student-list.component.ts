8348d6c500d678cdcdb78006aa0720375d6e8496
damian.figinski <figo96it@gmail.com>
2024-04-25 19:20:25 +0200

    Add exceptions for students list

--- src/app/student-list/student-list.component.ts
+++ src/app/student-list/student-list.component.ts
@@ -2,6 +2,8 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { Student } from '../student';
 import { HttpStudentService } from '../http-student.service';
+import { delay } from 'rxjs';
+import { animate } from '@angular/animations';
 
 @Component({
   selector: 'app-student-list',
@@ -12,19 +14,41 @@ import { HttpStudentService } from '../http-student.service';
 })
 export class StudentListComponent {
   isTableVisible: boolean = true;
+  isDataLoaded = false;
+  isErrorOccured = false;
   students: Student[] = [
-    new Student(1, "Jan Kowalski", "kowalski@gmail.com"),
-    new Student(2, "Adam Nowak", "nowak@onet.pl"),
-    new Student(3, "Joanna Nowak", "joanna@o2.pl"),
-    new Student(27, "Karolina Kowalska", "kowalska@gmail.com"),
-    new Student(5, "Ola Olowska", "ola@gmail.com"),
-    new Student(61, "Piotr Testowy", "testowy@gmail.com")
+    // new Student(1, "Jan Kowalski", "kowalski@gmail.com"),
+    // new Student(2, "Adam Nowak", "nowak@onet.pl"),
+    // new Student(3, "Joanna Nowak", "joanna@o2.pl"),
+    // new Student(27, "Karolina Kowalska", "kowalska@gmail.com"),
+    // new Student(5, "Ola Olowska", "ola@gmail.com"),
+    // new Student(61, "Piotr Testowy", "testowy@gmail.com")
   ];
 
+  //prosty model subscribe obsługuje tylko pozytywną odpowiedź z serwera
+  // constructor(private httpStudentService: HttpStudentService) {
+  //   console.log("Przed wywołaniem httpService");
+  //   this.httpStudentService.getStudents().pipe(delay(2000)).subscribe(data => {
+  //     console.log("Wewnątrz httpService");
+  //     this.students = data;
+  //     this.isDataLoaded = true;
+  //   });
+  //   console.log("Po wywołaniu httpService");
+  // }
+
   constructor(private httpStudentService: HttpStudentService) {
-    this.httpStudentService.getStudents().subscribe(data => {
-      this.students = data;
+    console.log("Przed wywołaniem httpService");
+    this.httpStudentService.getStudents().pipe(delay(2000)).subscribe({
+      next: data => {
+        console.log("Wewnątrz httpService");
+        this.students = data;
+        this.isDataLoaded = true;
+      }, error: _ => {
+        this.isErrorOccured = true;
+        this.isDataLoaded = true;
+      }
     });
+    console.log("Po wywołaniu httpService");
   }
 
   toogleDisplay() {
