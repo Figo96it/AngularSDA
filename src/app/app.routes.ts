68bc93c330a9f0f1b6e638c4d311579d7a6be11a
damian.figinski <figo96it@gmail.com>
2024-05-07 17:39:18 +0200

    add new component add-student

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { SandboxComponent } from './sandbox/sandbox.component';
 import { StudentListComponent } from './student-list/student-list.component';
+import { AddStudentComponent } from './add-student/add-student.component';
 
 export const routes: Routes = [
     { path: "home", component: HomeComponent },
     { path: "sandbox", component: SandboxComponent },
-    { path: "students", component: StudentListComponent }
+    { path: "students", component: StudentListComponent },
+    { path: "add-student", component: AddStudentComponent }
 ];
